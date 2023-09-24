347d650cabae89aac407b5219271214a5400662b
Aheed Shah <aheed@duck.com>
2023-09-24 12:12:57 +0100

    Small changes

--- src/components/Home/Youtube/Youtube.jsx
+++ src/components/Home/Youtube/Youtube.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './Youtube.scss';
 
 const Youtube = () => {
-    return(
-        <div className='youtube-container'>
-            <h1>YOU&apos;LL LOVE WHY WE&apos;RE DIFFERENT. TAKE A LOOK</h1>
-            <hr className='hr-youtube' />
-            <p>Toddler World is renowned for providing rich learning opportunities in unique and exciting environment. </p>
+    return (
+        <div className="youtube-container">
+            <h1>YOU&apos;LL LOVE WHY WE'RE DIFFERENT. TAKE A LOOK</h1>
+            <hr className="hr-youtube"/>
+            <p>Toddler World is renowned for providing rich learning opportunities in unique and exciting
+				environment. </p>
 
-            <iframe className='youtube-video' src="https://www.youtube.com/embed/FxxsA-qaHTM"></iframe>
+            <iframe className="youtube-video" src="https://www.youtube.com/embed/FxxsA-qaHTM"></iframe>
         </div>
     );
 };

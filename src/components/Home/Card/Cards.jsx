daa05b756f0ce2e7ed74a41fa0bac82b238673a4
Aheed Shah <aheed@duck.com>
2023-09-26 19:52:50 +0100

    Adding a carousel, images and small changes

--- src/components/Home/Card/Cards.jsx
+++ src/components/Home/Card/Cards.jsx
@@ -4,28 +4,28 @@ import CardItem from '../../CardItem/CardItem';
 
 function Cards() {
     return (
-        <div className='cards'>
+        <div className="cards">
             <h1>A bit about us</h1>
-            <div className='card'>
-                <div className='wrapper'>
-                    <ul className='items'>
+            <div className="card">
+                <div className="wrapper">
+                    <ul className="items">
                         <CardItem
-                            src='/gifs/placeholder1.gif'
+                            src="/gifs/placeholder1.gif"
                             text={'Our well-designed environment supports child\'s growth and development'}
-                            label='Our Environment'
-                            path='/our-environment'
+                            label="Our Environment"
+                            path="/our-environment"
                         />
                         <CardItem
-                            src='/gifs/placeholder2.gif'
-                            text='World class infrastructure for holistic development of the child'
-                            label='Our Facilities'
-                            path='/play-equipment'
+                            src="/gifs/placeholder2.gif"
+                            text="World class infrastructure for holistic development of the child"
+                            label="Our Facilities"
+                            path="/play-equipment"
                         />
                         <CardItem
-                            src='/gifs/placeholder3.gif'
-                            text='Our age appropriate programs that grow with your child'
-                            label='Our Programmes'
-                            path='/login'
+                            src="/gifs/placeholder3.gif"
+                            text="Our age appropriate programmes that grow with your child"
+                            label="Our Programmes"
+                            path="/login"
                         />
                     </ul>
                 </div>

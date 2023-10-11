a6a822f411723349fd45938d6f8ca528f7bee770
Aheed Shah <aheed@duck.com>
2023-10-11 22:53:54 +0100

    chore: styling issues fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,48 +1,55 @@
 import React from 'react';
 import './About.scss';
-import {Button} from '../Button/Button';
+// import {Button} from '../Button/Button';
 
 function About () {
     return (
         <div className='about-container' data-testid='about-container'>
             <h1 className='heading'>About us</h1>
             <hr className='hr-about-us' />
-            <div className='about-section-1'>
+            <div className='text-block mobile image-left'>
                 <img src='/images/about/first.webp' alt='Teacher showing how to write to a child'/>
+                {/*<div className='about-text'>*/}
                 <h1>Our Team</h1>
-                <div className='about-button'>
-                    <Button
-                        buttonStyle='primary-button'
-                        buttonSize='large'
-                    >
-                        Learn More
-                    </Button>
-                </div>
+                {/*<div className='about-button'>*/}
+                {/*    <Button*/}
+                {/*        buttonStyle='primary-button'*/}
+                {/*        buttonSize='large'*/}
+                {/*    >*/}
+                {/*        Learn More*/}
+                {/*    </Button>*/}
+                {/*</div>*/}
+                {/*</div >*/}
             </div>
-            <div className='about-section-2'>
+            <div className='text-block mobile image-right'>
                 <img src='/images/about/second.webp' alt='Children sitting on chairs and reading books' />
+                {/*<div className='about-text-left'>*/}
                 <h1>Our Team</h1>
-                <div className='about-button'>
-                    <Button
-                        buttonStyle='primary-button'
-                        buttonSize='large'
-                    >
-                        Learn More
-                    </Button>
-                </div>
+                {/*<div className='about-button'>*/}
+                {/*    <Button*/}
+                {/*        buttonStyle='primary-button'*/}
+                {/*        buttonSize='large'*/}
+                {/*    >*/}
+                {/*        Learn More*/}
+                {/*    </Button>*/}
+                {/*</div>*/}
+                {/*</div>*/}
             </div>
-            <div className='about-section-3'>
+
+            <div className='text-block mobile image-left'>
                 <img src='/images/about/third.webp' alt='Nine Children playing together'/>
+                {/*<div className='about-text'>*/}
                 <h1>Our Team</h1>
-                <div className='about-button'>
-                    <Button
-                        buttonStyle='primary-button'
-                        buttonSize='large'
-                    >
-                        Learn More
-                    </Button>
-                </div>
+                {/*<div className='about-button'>*/}
+                {/*<Button*/}
+                {/*    buttonStyle='primary-button'*/}
+                {/*    buttonSize='large'*/}
+                {/*>*/}
+                {/*    Learn More*/}
+                {/*</Button>*/}
+                {/*</div>*/}
             </div>
+            {/*</div>*/}
         </div>
     );
 }

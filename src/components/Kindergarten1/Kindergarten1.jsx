9a95acfa40d2d866a7b98eadd83166e1f5c99983
Aheed Shah <aheed@duck.com>
2023-10-24 10:20:56 +0100

    feat: add Kindergarten1&2 and Nursery component

---
+++ src/components/Kindergarten1/Kindergarten1.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Button} from '../Button/Button';
+import './Kindergarten1.scss';
+
+const Kindergarten1 = () => {
+    return(
+        <>
+            <div className='heading-kindergarten1'>
+                <img src='/images/kindergarten1/1.webp' alt='A girl who has her hands open' className='kindergarten1-image'/>
+                <h1 className='heading'>KINDERGARTEN 1 (4 - 5 YEARS)</h1>
+                <hr className='hr-about-us' />
+            </div>
+            <div className='kindergarten1-container'>
+                <div className='left-aligned-kindergarten1'>
+                    <p>
+                        <strong>Skills that will guide your child towards future academic success:
+                            <i>Kindergarten 1 at Toddler World.</i>
+                        </strong><br /><br />
+
+                        This program is meant for children between 4 years to 5 years old. In Kindergarten 1 we focus
+                        on skills that will guide your child towards future academic success. The learning tenets of the
+                        program include social finesse and mastering gross motor skills. Each school day we provide your
+                        child with opportunities for independent discovery, cooperative learning and generous outdoor
+                        playtime.
+                    </p>
+                </div>
+                <div className='right-aligned-kindergarten1'>
+                    <h1>Learn More</h1>
+                    <p>There is a lot more to know. Please feel free to contact us.
+                    </p>
+                    <div className='contact-us-kindergarten1' data-testid="contact-us-comp">
+                        <a href='/contact-us' data-testid="contact-us-link-test">
+                            <Button
+                                buttonStyle="primary-button"
+                                buttonSize="large"
+                                data-testid="contact-us-test"
+                            >
+                                Contact us
+                            </Button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Kindergarten1;
---
+++ src/components/Kindergarten1/Kindergarten1.test.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Kindergarten1 from './Kindergarten1';
+
+describe('Kindergarten1 Component Tests', () => {
+    test('renders  Kindergarten1 component', () => {
+        render(<Kindergarten1 />);
+        expect(screen.getByText('KINDERGARTEN 1 (4 - 5 YEARS)')).toBeInTheDocument();
+    });
+
+    test('renders the image for  Kindergarten1 component', () => {
+        const { getByAltText } = render(<Kindergarten1 />);
+        const image = getByAltText('A girl who has her hands open');
+        expect(image).toBeInTheDocument();
+    });
+
+    test('renders the left-aligned Kindergarten1 content', () => {
+        const { getByText } = render(<Kindergarten1 />);
+        const leftContent = getByText(/This program is meant for children between/i);
+        expect(leftContent).toBeInTheDocument();
+    });
+
+    test('renders the right-aligned Pre Kindergarten1 content', () => {
+        const { getByText } = render(<Kindergarten1 />);
+        const rightContent = getByText(/There is a lot more to know/i);
+        expect(rightContent).toBeInTheDocument();
+    });
+
+    test('renders the "Learn More" section', () => {
+        const { getByText } = render(<Kindergarten1 />);
+        const learnMoreHeading = getByText('Learn More');
+        const learnMoreText = getByText(/There is a lot more to know/i);
+        expect(learnMoreHeading).toBeInTheDocument();
+        expect(learnMoreText).toBeInTheDocument();
+    });
+
+    test('button click should open the contact us link', () => {
+        const {container} = render(<Kindergarten1 />);
+
+        const registerButton = container.querySelector('[data-testid="contact-us-comp"]');
+
+        expect(registerButton).toBeInTheDocument();
+
+        const linkElement = screen.getByTestId("contact-us-link-test");
+
+        expect(linkElement).toBeInTheDocument();
+        expect(linkElement).toHaveAttribute(
+            'href',
+            '/contact-us'
+        );
+    });
+});

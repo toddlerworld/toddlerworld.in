d821363fd8c66cafc95c9eda19a54e59b866d5cb
Aheed Shah <aheed@duck.com>
2023-11-04 09:59:16 +0000

    chore

--- src/components/Home/Youtube/Youtube.jsx
+++ src/components/Home/Youtube/Youtube.jsx
@@ -6,8 +6,9 @@ const Youtube = () => {
         <div className="youtube-container" data-testid="youtube-component">
             <h1>YOU&apos;LL LOVE WHY WE&apos;RE DIFFERENT. TAKE A LOOK</h1>
             <hr className="hr-youtube"/>
-            <p>Toddler World is renowned for providing rich learning opportunities in unique and exciting
-				environment. </p>
+            <p>
+                Toddler World is renowned for providing rich learning opportunities in unique and exciting environment.
+            </p>
 
             <div className="youtube-video-container">
                 <iframe className="youtube-video" src="https://www.youtube.com/embed/FxxsA-qaHTM" data-testid="youtube-video"></iframe>
